9e05fbba31570ddd97f34df54fa57f963044c77c
RookieGamer465 <87334432+RookieGamer465@users.noreply.github.com>
2022-09-06 17:52:05 +0100

    Add files via upload

---
+++ usergen.py
@@ -0,0 +1,10 @@
+# extension task
+# charley mckay
+
+# gather information from user, change string to full capitals
+firstname = str.upper(input(please enter your first name: ))
+surname = str.upper(input(please enter your second name: ))
+
+# construct the username with the given and calculated information
+username = str(ord(firstname[0:1]))+str(ord(surname[-1:]))+firstname[0:5]+surname[0:5]+str(ord(surname[0:1]))
+print(your generated username is: {}.format(username))
